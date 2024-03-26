380285937b040ddb17874688260d192ec518234f
Isaac Lee <2starwook@gmail.com>
2024-03-26 17:37:59 -0500

    Initial Commit

---
+++ AddRecordViewModelTests.cs
@@ -0,0 +1,26 @@
+﻿using Object.Frontend;
+
+namespace UniteTestProject;
+
+public class AddRecordViewModelTests
+{
+    [Fact]
+    public void RefreshTest()
+    {
+        //Arrange
+        var viewModel = new AddRecordViewModel();
+        viewModel.FileButtonText = "Random";
+        viewModel.RecordName = "Record1";
+        viewModel.FilePath = "./random.wav";
+        viewModel.SelectedPatient = new PatientInfo("name", "id");
+
+        //Act
+        viewModel.Refresh();
+
+        //Assert
+        viewModel.FileButtonText.Should().Be("Select a File");
+        viewModel.RecordName.Should().BeEmpty();
+        viewModel.FilePath.Should().BeEmpty();
+        viewModel.SelectedPatient.Should().BeNull();
+    }
+}
---
+++ Usings.cs
@@ -0,0 +1,3 @@
+﻿global using Xunit;
+global using NET_MAUI_BLE.ViewModel;
+global using FluentAssertions;
